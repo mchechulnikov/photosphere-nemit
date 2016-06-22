59cc75b4f4105bd29a92e70be7bede5a98d03fb1
Максим Чечульников <ilfirin.nar@gmail.com>
2016-06-23 00:00:52 +0300

    Not working test

---
+++ src/Photosphere.NativeEmit.Tests/FirstTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Photosphere.NativeEmit.Tests
+{
+    public class FirstTests
+    {
+        [Fact]
+        internal void Test()
+        {
+            Assert.Equal(42, Program.Get42());
+        }
+    }
+}
--- src/Photosphere.NativeEmit/Program.cs
+++ src/Photosphere.NativeEmit/Program.cs
@@ -1,33 +1,27 @@
 using System;
-using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using Photosphere.NativeEmit.External;
+using Photosphere.NativeEmit.x86;
+using Photosphere.NativeEmit.x86.Enums;
 
 namespace Photosphere.NativeEmit
 {
     // generate native method that returns 42
     internal class Program
     {
-        const uint PageExecuteReadwrite = 0x40;
-        const uint MemCommit = 0x1000;
-
-        
+        private const uint PageExecuteReadwrite = 0x40;
+        private const uint MemCommit = 0x1000;
 
         private delegate int IntReturner();
 
-        internal static void Main(string[] args)
+        public static int Get42()
         {
-            var bodyBuilder = new List<byte>();
-            bodyBuilder.Add(0xb8);
-            bodyBuilder.AddRange(BitConverter.GetBytes(42));
-            bodyBuilder.Add(0xc3);
-            var body = bodyBuilder.ToArray();
+            var body = new Emitter().Mov(Register.Eax, 42).Ret().ToArray();
             var buf = Kernel32.VirtualAlloc(IntPtr.Zero, (uint)body.Length, MemCommit, PageExecuteReadwrite);
             Marshal.Copy(body, 0, buf, body.Length);
 
-            var ptr = (IntReturner)Marshal.GetDelegateForFunctionPointer(buf, typeof(IntReturner));
-            Console.WriteLine(ptr());
-            Console.ReadKey();
+            var ptr = (IntReturner) Marshal.GetDelegateForFunctionPointer(buf, typeof(IntReturner));
+            return ptr();
         }
     }
 }
